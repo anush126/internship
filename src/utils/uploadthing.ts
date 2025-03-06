c3afb02a0f8a381dae619c228474de54df58662c
anush126 <anushsubedar007@gmail.com>
2025-03-06 16:45:39 +0530

    aa

--- src/utils/uploadthing.ts
+++ src/utils/uploadthing.ts
@@ -5,7 +5,6 @@ import {
   } from "@uploadthing/react";
   
   import type { OurFileRouter } from "src/app/api/uploadthing/core";
-  
-  export const UploadButton = generateUploadButton<OurFileRoutr>();
+  export const UploadButton = generateUploadButton<OurFileRouter>();
   export const UploadDropzone = generateUploadDropzone<OurFileRouter>();
   
